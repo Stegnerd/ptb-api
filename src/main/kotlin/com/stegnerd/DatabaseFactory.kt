1166530933b59a5f60d75c870c915895f763bdd5
Dalton Stegner <dmstegner@gmail.com>
2021-10-01 00:03:50 -0400

    fixed typo

--- src/main/kotlin/com/stegnerd/DatabaseFactory.kt
+++ src/main/kotlin/com/stegnerd/DatabaseFactory.kt
@@ -15,7 +15,7 @@ object DatabaseFactory {
     private val appConfig = HoconApplicationConfig(ConfigFactory.load())
     private val dbUrl = appConfig.property("db.jdbcUrl").getString()
     private val dbUser = appConfig.property("db.dbUser").getString()
-    private val dbPassword = appConfig.property("db.dbPassword").toString()
+    private val dbPassword = appConfig.property("db.dbPassword").getString()
 
     fun init() {
         Database.connect(hikari())
