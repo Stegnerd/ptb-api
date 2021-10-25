ff6071e641f1a5f71e93507d59eeeec7c2fc919a
Dalton Stegner <dmstegner@gmail.com>
2021-10-25 19:52:20 -0400

    get auth working

--- src/main/kotlin/com/stegnerd/plugins/Security.kt
+++ src/main/kotlin/com/stegnerd/plugins/Security.kt
@@ -9,6 +9,10 @@ import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.features.CORS
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.ForwardedHeaderSupport
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import org.koin.ktor.ext.inject
 
 fun Application.configureSecurity() {
@@ -21,7 +25,21 @@ fun Application.configureSecurity() {
         authenticationModule(config.jwtRealm,userApi, databaseProvider, jwtVerifier)
     }
 
+    install(ForwardedHeaderSupport)
+    install(DefaultHeaders)
     install(CORS){
+        method(HttpMethod.Options)
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        header(HttpHeaders.AccessControlAllowHeaders)
+        header(HttpHeaders.AccessControlAllowOrigin)
+        header(HttpHeaders.Authorization)
+        header(HttpHeaders.ContentType)
+        allowCredentials = true
+        allowSameOrigin = true
+        allowNonSimpleContentTypes = true
         anyHost()
     }
 }
